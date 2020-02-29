d6fa3a25e1acbbe511ef713919db1c18e9a9de66
Tokotype <abc@tokotypefaces.com>
2020-02-29 15:14:33 +0700

    added scroll nav

--- assets/script.js
+++ assets/script.js
@@ -68,4 +68,14 @@ function navBar(){
   jQuery('body').scrollspy({target: ".navbar", offset: 0});   
 };
 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-500px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
